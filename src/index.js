a90dd29db5d7a7d7d0e7eb5c1f130a883c98777e
Roberto Cestari <robertotcestari@gmail.com>
2022-05-04 17:58:41 -0300

    initial commit 🐶

---
+++ src/index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+const playSong = require("./playSong");
+
+
+console.log(" ");
+
+console.log("= = = = = = =");
+console.log("Feito carinhosamente para a Turma 21!");
+console.log(" ");
+console.log("BOA TARDE TURMA 21");
+console.log("= = = = = = =");
+console.log(" ");
+console.log(" ");
+
+playSong('boatardeturma21')
